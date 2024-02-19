f03725bd80868dc6213ff9764df9bc0aed3030e6
darrenhjle <148319649+darrenhjle@users.noreply.github.com>
2024-02-19 14:57:40 +0800

    comments added for javadoc

--- Account.java
+++ Account.java
@@ -1,3 +1,11 @@
+/**
+ * Author: Darren, Min xuan, Monika, Teren, Jana, Amanda, Kirby
+ * E-mail: -
+ * Date: 20240219
+ *
+ * Description: The Account class represents a basic financial account with deposit,
+ * withdrawal, and balance operations, as well as transfer operations to other accounts.
+ */
 public class Account{
     private int accountNumber;
     private double balance;
@@ -15,39 +23,99 @@ public class Account{
         this.transferLimit = 1000;
         this.debt = 0;
     }
+    /**
+    * Gets the account number of the account.
+    *
+    * @return The account.
+    */
     public int getAccountNumber(){
         return accountNumber;
     }
+    /**
+     * Sets the account number of the account.
+     * 
+     * @param accountNumber The account number.
+     */
     public void setAccountNumber(int accountNumber){
         this.accountNumber = accountNumber;
     }
+    /**
+    * Gets the current balance of the account.
+    *
+    * @return The current balance.
+    */
     public double getBalance(){
         return balance;
     }
+    /**
+     * Sets the current balance of the account.
+     *  
+     * @param balance The current balance.
+     */
     public void setBalance(double balance){
         this.balance = balance;
     }
+    /**
+     * Gets the PIN of the account.
+     * 
+     * @return The PIN.
+     */
     public int getPIN(){
         return PIN;
     }
+    /**
+     * Sets the PIN of the account.
+     * 
+     * @param PIN The PIN.
+     */
     public void setPIN(int PIN){
         this.PIN = PIN;
     } 
+    /**
+     * Gets the withdraw limit of the account.
+     * 
+     * @return The withdraw limit.
+     */
     public double getWithdrawLimit(){
         return withdrawLimit;
     }
+    /**
+     * Sets the withdraw limit of the account.
+     * 
+     * @param withdrawLimit The withdraw limit.
+     */
     public void setWithdrawLimit(double withdrawLimit){
         this.withdrawLimit = withdrawLimit;
     }
+    /**
+     * Gets the transfer limit of the account.
+     * 
+     * @return The transfer limit.
+     */
     public double getTransferLimit(){
         return transferLimit;
     }
+    /**
+     * Sets the transfer limit of the account.
+     * 
+     * @param transferLimit The transfer limit.
+     */
     public void setTransferLimit(double transferLimit){
         this.transferLimit = transferLimit;
     } 
+    /**
+    * Deposits the specified amount into the account.
+    *
+    * @param amount The amount to deposit.
+    */
     public void deposit(double amount){
         balance += amount;
     }
+    /**
+     * Withdraws the specified amount from the account.
+     * 
+     * @param amount The amount to withdraw.
+     */
     public void withdraw(double amount){
         if(amount > withdrawLimit){
             System.out.println("Withdraw limit exceeded");
@@ -59,6 +127,11 @@ public class Account{
             balance -= amount;
         }
     }
+    /**
+     * Transfers the specified amount from the account to another account.
+     * 
+     * @param account The account to transfer to.
+     */
     public void interAccountTransfer(Account account, double amount){
         if(amount > transferLimit){
             System.out.println("Transfer limit exceeded");
@@ -71,6 +144,11 @@ public class Account{
             account.deposit(amount);
         }
     }
+    /**
+     * Transfers the specified amount from the account to a third party account.
+     * 
+     * @param account The account to transfer to.
+     */
     public void thirdPartyTransfer(Account account, double amount){
         if(amount > transferLimit){
             System.out.println("Transfer limit exceeded");
