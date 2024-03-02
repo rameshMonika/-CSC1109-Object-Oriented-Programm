77de81c7e0946d0f9f38050e73f3324d270b34e1
amanda-chek <amandachek.acyt@gmail.com>
2024-03-03 03:00:44 +0800

    added JavaDoc comments for methods & remove main

--- Exchange.java
+++ Exchange.java
@@ -1,13 +1,24 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/** The Exchange class represents a currency exchange system with predefined exchange rates. */
 public class Exchange {
+
+    /**
+     * Enum representing different currencies.
+    */
     public enum Currency {
         USD, JPY, THB, MYR
     }
 
-    Map<Currency, Double> currencyMap;
+    /**
+     * A map to store exchange rates for different currencies.
+    */
+    private Map<Currency, Double> currencyMap;
 
+    /**
+     * Default constructor that initializes the Exchange object with predefined exchange rates.
+    */
     public Exchange() {
         currencyMap = new HashMap<>();
         currencyMap.put(Currency.USD, 0.68653311);
@@ -16,24 +27,16 @@ public class Exchange {
         currencyMap.put(Currency.MYR, 3.5286746);
     }
 
-    void getRate(Currency c) {
+    /**
+     * Retrieves and prints the exchange rate for a given currency.
+     *
+     * @param c The currency for which the exchange rate is requested.
+     */
+    public void getRate(Currency c) {
         if (currencyMap.containsKey(c)) {
             System.out.println("Exchange rate for " + c + ": " + currencyMap.get(c));
         } else {
             System.out.println("Exchange rate not available for " + c);
         }
     }
-
-    // (Optional) - to show all currencies 
-    void printAllCurrencies() {
-        System.out.println("Exchange Rates:");
-        for (Map.Entry<Currency, Double> entry : currencyMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-    }
-
-    // public static void main(String[] args) {
-    // Exchange exchange = new Exchange();
-    // exchange.getRate(Currency.MYR);
-    // }
 }
