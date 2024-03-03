a9c6bd92d7965c77e13a7753349e9097f65c4a20
amanda-chek <amandachek.acyt@gmail.com>
2024-03-03 23:00:27 +0800

    Change enum to String

--- Exchange.java
+++ Exchange.java
@@ -1,5 +1,5 @@
 /**
- * Description: Represents a currency exchange system with predefined exchange rates.
+ * Description: The Exchange class represents a currency exchange system with predefined exchange rates.
  */
 
 // Imported libraries
@@ -8,24 +8,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The Exchange class facilitates currency exchange with predefined exchange
- * rates.
+ * The Exchange class facilitates currency exchange with predefined exchange rates.
  * It supports four different currencies: USD, JPY, THB, and MYR.
  */
-
 public class Exchange {
-
-    /**
-     * Enum representing different currencies.
-     */
-    public enum Currency {
-        USD, JPY, THB, MYR
-    }
-
     /**
      * A map to store exchange rates for different currencies.
      */
-    private Map<Currency, Double> currencyMap;
+    private Map<String, Double> currencyMap;
 
     /**
      * Default constructor that initializes the Exchange object with predefined
@@ -41,10 +31,10 @@ public class Exchange {
      */
     public Exchange() {
         currencyMap = new HashMap<>();
-        currencyMap.put(Currency.USD, 0.68653311);
-        currencyMap.put(Currency.JPY, 111.67136);
-        currencyMap.put(Currency.THB, 26.69023);
-        currencyMap.put(Currency.MYR, 3.5286746);
+        this.currencyMap.put("USD", 0.68653311);
+        this.currencyMap.put("EUR", 0.69);
+        this.currencyMap.put("JPY", 111.67136);
+        this.currencyMap.put("MYR", 3.5286746);
     }
 
     /**
@@ -52,7 +42,7 @@ public class Exchange {
      *
      * @param c The currency for which the exchange rate is requested.
      */
-    public void getRate(Currency c) {
+    public void getRate(String c) {
         if (currencyMap.containsKey(c)) {
             System.out.println("Exchange rate for " + c + ": " + currencyMap.get(c));
         } else {
