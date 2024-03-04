f01504bda27f1a224bb9df51d3bd688d2f01cdf0
darrenhjle <148319649+darrenhjle@users.noreply.github.com>
2024-03-04 19:50:28 +0800

    javadoc for creditcard loan and exchange

---
+++ javadoc for the 3 class/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Exchange","l":"convertCurrency(Currency, Double)","u":"convertCurrency(Currency,java.lang.Double)"},{"p":"<Unnamed>","c":"Exchange","l":"Exchange()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Exchange","l":"getRate(Currency)"}];updateSearchResults();
