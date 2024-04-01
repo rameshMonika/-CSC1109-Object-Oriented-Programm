d2859ec813346613b981174983d39c84c109d4c5
terentan <64367199+terentan@users.noreply.github.com>
2024-04-02 00:47:52 +0800

    Update LoanTest.java
    updated with exceptionHandling

--- LoanTest.java
+++ LoanTest.java
@@ -1,52 +1,50 @@
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class LoanTest {
     public static void main(String[] args) {
-        // Create a Customer
 
-        Customer customer = new Customer("S1234567A", "John Doe", new Date(), 12345678, "john.doe@example.com", 30, 50000.0);
+        try {
+            // Create a Customer
 
-        // Create an Account for the Customer
-        Account account = new Account(customer, 123456, 1234);
+            Customer customer = new Customer("S1234567A", "John Doe", new Date(), 12345678, "john.doe@example.com", 30, 50000.0);
 
-       BusinessLoan businessLoan = new BusinessLoan("Technology", "Software Development", 75000.0, 0.05f, 12, "Business Loan", "Jake",2,220000.00,93887001, account, "EN123456", 200000.0, 100000, 50000, "Pending");
+            // Create an Account for the Customer
+            Account account = new Account(customer, 123456, 1234);
 
-        BusinessLoan businessLoan2 = new BusinessLoan("Food", "Fast", 75000.0, 0.05f, 12, "Business Loan", "Jake",2,220000.00,93887001, account, "EN123456", 200000.0, 100000, 50000, "Pending");
+            BusinessLoan businessLoan = new BusinessLoan("Technology", "Software Development", 75000.0, 0.05f, 12, "Business Loan", "Jake",2,220000.00,93887001, account, "EN123456", 200000.0, 100000, 50000, "Pending");
 
-        PersonalLoan personalLoan = new PersonalLoan(5000.0, 0.1f, 24, "Personal Loan", "Jane", 3, 30000.0, 98765432, account, "Pending", 3000.0, 36000.0);
+            BusinessLoan businessLoan2 = new BusinessLoan("Food", "Fast", 75000.0, 0.05f, 12, "Business Loan", "Jake",2,220000.00,93887001, account, "EN123456", 200000.0, 100000, 50000, "Pending");
 
-         // Create a Study Loan
-        StudyLoan studyLoan = new StudyLoan(10000.0, 0.05f, 36, "Study Loan", "Jane", 3, 30000.0, 98765432, account, "STU123456", "University ABC", "Pending");
+            PersonalLoan personalLoan = new PersonalLoan(5000.0, 0.1f, 24, "Personal Loan", "Jane", 3, 30000.0, 98765432, account, "Pending", 3000.0, 36000.0);
 
+            // Create a Study Loan
+            StudyLoan studyLoan = new StudyLoan(10000.0, 0.05f, 36, "Study Loan", "Jane", 3, 30000.0, 98765432, account, "STU123456", "University ABC", "Pending");
 
+            // Set the Business Loan for the Account
 
+            account.addLoan(businessLoan);
+            account.addLoan(businessLoan2);
+            account.addLoan(personalLoan);
+            account.addLoan(studyLoan);
 
+            // Create a Bank
+            Bank.setBankname("Example Bank");
 
-        // Set the Business Loan for the Account
+            // Add the Account to the Bank
+            Bank.addAccount(account);
+            }catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }catch(Exception e){
+                System.out.println("Exception: " + e.getMessage());
+            }
 
-        account.addLoan(businessLoan);
-        account.addLoan(businessLoan2);
-        account.addLoan(personalLoan);
-        account.addLoan(studyLoan);
-
-        
-
-        // Create a Bank
-        Bank.setBankname("Example Bank");
-
-        // Add the Account to the Bank
-        Bank.addAccount(account);
-
-      
-
-
-  
 
         System.out.println("=======================================================================================");
-         ArrayList<Loan> pendingLoans = Bank.getPendingLoans();
+        //ArrayList<Loan> pendingLoans = Bank.getPendingLoans();
         System.out.println("\nPersonal Loans:");
         ArrayList<Loan> pendingPersonalLoans = Bank.getPendingLoansByType("Personal Loan");
         for (Loan loan : pendingPersonalLoans) {
@@ -54,9 +52,7 @@ public class LoanTest {
             System.out.println("Loan ID: " + loan.getLoanID());
             System.out.println("Loan Type: " + loan.getLoanType());
             System.out.println("Loan Status: " + loan.getLoanStatus());
-            
             System.out.println("----------------------------------------------------------------------------");
-
         }
 
         // Filter and print pending business loans
@@ -68,7 +64,6 @@ public class LoanTest {
             System.out.println("Loan Type: " + loan.getLoanType());
             System.out.println("Loan Status: " + loan.getLoanStatus());
             System.out.println("----------------------------------------------------------------------------");
-
         }
 
         // Filter and print pending study loans
@@ -80,17 +75,18 @@ public class LoanTest {
              System.out.println("Loan Type: " + loan.getLoanType());
             System.out.println("Loan Status: " + loan.getLoanStatus());
             System.out.println("----------------------------------------------------------------------------");
-
-          
-
-     
-
         }
-
+        
+        int loanID = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the loan ID to review details: ");
-        int loanID = scanner.nextInt();
-        scanner.nextLine();
+        try{
+            System.out.print("Enter the loan ID to review details: ");
+            String userInt = scanner.nextLine();
+            loanID = Integer.parseInt(userInt);
+        }catch (InputMismatchException | NumberFormatException e) {
+            System.out.println("Please enter a a proper Loan ID!"); 
+            scanner.nextLine();
+        }
 
  
      
@@ -106,14 +102,10 @@ public class LoanTest {
             }
 
             if (loanToReview != null) {
-            
-
-               
-
                 if (loanToReview instanceof PersonalLoan) {
-                
+      
                  PersonalLoan personalLoanInstance = (PersonalLoan) loanToReview;
-                 personalLoan.printLoanDetails();
+                 personalLoanInstance.printLoanDetails();
                
                 }
                 
@@ -130,21 +122,21 @@ public class LoanTest {
                 
                 }
                 System.out.print("Do you want to approve (A) or reject (R) this loan? :");
-                String decision = scanner.nextLine();
+                String decision = scanner.nextLine().toUpperCase();
+                scanner.close();
+                if(!decision.equals("A") && !decision.equals("R")){
+                    throw new IllegalArgumentException("Please enter either 'A' to approve or 'R' to reject.");
+                }
 
                 if (decision.equals("A")) {
                     loanToReview.approveLoan();
                 } else if (decision.equals("R")) {
                     loanToReview.rejectLoan();
                 } 
-
-                
-
-
-
                  
+            } else{
+                System.out.println("Loan ID " + loanID + " not found.");
             }
-
         
     }
 }
