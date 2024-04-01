55b07367a902ebb90fb1618de75738876ca7ea5e
amanda-chek <amandachek.acyt@gmail.com>
2024-04-02 01:31:22 +0800

    remove import library from currency

--- Currency.java
+++ Currency.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public enum Currency {
     AED, AFN, ALL, AMD, ANG, AOA, ARS, AUD, AWG, AZN, BAM, BBD, BDT, BGN, BHD, BIF, BMD, BND, BOB, BRL, BSD, BTN, BWP,
     BYN, BZD, CAD, CDF, CHF, CLP, CNY, COP, CRC, CUP, CVE, CZK, DJF, DKK, DOP, DZD, EGP, ERN, ETB, EUR, FJD, FKP, FOK,
